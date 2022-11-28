e1bed85b36044c0f2553bed671a5f629579bad21
SydneySmoothvile <sidneybradleyabuya@gmail.com>
2022-11-28 19:10:29 +0300

    Managed to classify 1

--- severe/views.py
+++ severe/views.py
@@ -10,7 +10,7 @@ def home(request):
     return render(request, 'index.html')
 
 
-def getClassifier(RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_X_rest, RPC_Y_rest, RPC_Z_rest, TSI_kin, TSI_amplitude_kin, TSI_rest, TSI_amplitude_rest):
+def getClassifier(RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_U_kin, RPC_X_rest, RPC_Y_rest, RPC_Z_rest, RPC_U_rest, TSI_kin, TSI_amplitude_kin, Mean_Peak_Amplitude_kin, Stddev_Peak_Amplitude_kin, TSI_rest, TSI_amplitude_rest, Mean_Peak_Amplitude_rest, Stddev_Peak_Amplitude_rest):
     model = pickle.load(open('tremor_classifier_model.joblib', 'rb'))
     scaled = pickle.load(open('scaler.sav', 'rb'))
     # ['RPC_X_kin',
@@ -31,8 +31,8 @@ def getClassifier(RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_X_rest, RPC_Y_rest, RPC_Z
     #  'Stddev Peak Amplitude_+rest']
 
     prediction = model.predict(scaled.transform([
-        [RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_X_rest, RPC_Y_rest, RPC_Z_rest,
-            TSI_kin, TSI_amplitude_kin, TSI_rest, TSI_amplitude_rest,]
+        [RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_U_kin, RPC_X_rest, RPC_Y_rest, RPC_Z_rest, RPC_U_rest, TSI_kin, TSI_amplitude_kin,
+            Mean_Peak_Amplitude_kin, Stddev_Peak_Amplitude_kin, TSI_rest, TSI_amplitude_rest, Mean_Peak_Amplitude_rest, Stddev_Peak_Amplitude_rest]
     ]))
     
     if prediction == 0:
@@ -46,15 +46,21 @@ def result(request):
     RPC_X_kin = float(request.GET['RPC_X_kin'])
     RPC_Y_kin = float(request.GET['RPC_Y_kin'])
     RPC_Z_kin = float(request.GET['RPC_Z_kin'])
+    RPC_U_kin = float(request.GET['RPC_U_kin'])
     RPC_X_rest = float(request.GET['RPC_X_rest'])
     RPC_Y_rest = float(request.GET['RPC_Y_rest'])
     RPC_Z_rest = float(request.GET['RPC_Z_rest'])
+    RPC_U_rest = float(request.GET['RPC_U_rest'])
     TSI_kin = float(request.GET['TSI_kin'])
     TSI_amplitude_kin = float(request.GET['TSI_amplitude_kin'])
+    Mean_Peak_Amplitude_kin = float(request.GET['Mean_Peak_Amplitude_kin'])
+    Stddev_Peak_Amplitude_kin = float(request.GET['Stddev_Peak_Amplitude_kin'])
     TSI_rest = float(request.GET['TSI_rest'])
     TSI_amplitude_rest = float(request.GET['TSI_amplitude_rest'])
+    Mean_Peak_Amplitude_rest = float(request.GET['Mean_Peak_Amplitude_rest'])
+    Stddev_Peak_Amplitude_rest = float(request.GET['Stddev_Peak_Amplitude_rest'])
 
-    result = getClassifier(RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_X_rest, RPC_Y_rest,
-                           RPC_Z_rest, TSI_kin, TSI_amplitude_kin, TSI_rest, TSI_amplitude_rest)
+    result = getClassifier(RPC_X_kin, RPC_Y_kin, RPC_Z_kin, RPC_U_kin, RPC_X_rest, RPC_Y_rest, RPC_Z_rest, RPC_U_rest, TSI_kin, TSI_amplitude_kin,
+                           Mean_Peak_Amplitude_kin, Stddev_Peak_Amplitude_kin, TSI_rest, TSI_amplitude_rest, Mean_Peak_Amplitude_rest, Stddev_Peak_Amplitude_rest)
 
     return render(request, 'result.html', {'result': result})
